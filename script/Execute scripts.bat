6150338d3bdb3dcb10fc182d416c0dac696f41fd
Lucas Bernardo de Souza Santos <sorackb@gmail.com>
2014-08-06 10:42:15 -0300

    Changes to better explain the command
    Changes to better explain the command

--- script/Execute scripts.bat
+++ script/Execute scripts.bat
@@ -1,4 +1,4 @@
-for %%G in (%~dp0\structure\*.sql) do sqlcmd /S 192.168.112.28\SQLEXPRESS /d fipe -U sa -P seguros1129! -i"%%G"
-for %%G in (%~dp0\programmability\*.sql) do sqlcmd /S 192.168.112.28\SQLEXPRESS /d fipe -U sa -P seguros1129! -i"%%G"
-for %%G in (%~dp0\data\*.sql) do sqlcmd /S 192.168.112.28\SQLEXPRESS /d fipe -U sa -P seguros1129! -i"%%G"
+for %%G in (%~dp0\structure\*.sql) do sqlcmd /S 127.0.0.1\SQLEXPRESS /d fipe -U sa -P *password* -i"%%G"
+for %%G in (%~dp0\programmability\*.sql) do sqlcmd /S 127.0.0.1\SQLEXPRESS /d fipe -U sa -P *password* -i"%%G"
+for %%G in (%~dp0\data\*.sql) do sqlcmd /S 127.0.0.1\SQLEXPRESS /d fipe -U sa -P *password* -i"%%G"
 pause
