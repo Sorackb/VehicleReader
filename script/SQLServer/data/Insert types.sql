c95cb9a843311c59fc75f443519f59ed5dfa7936
Sorackb <sorackb@gmail.com>
2017-01-20 09:30:55 -0200

    Insertion updated to a better solution

--- script/SQLServer/data/Insert types.sql
+++ script/SQLServer/data/Insert types.sql
@@ -1,142 +1,60 @@
-if not exists(select vc.id
-                from fipe.tb_vehicle_classification vc
-               where vc.id = 1)
-begin
-  insert into fipe.tb_vehicle_classification(id,
-                                             description_en,
-                                             description_pt)
-                                      values(1,
-                                             'Car',
-                                             'Passeio');
-end;
-go
+declare @classification table(id             int,
+                              description_en varchar(15),
+                              description_pt varchar(15));
+declare @fuel_type table(id             int,
+                         description_en varchar(30),
+                         description_pt varchar(30));
+declare @situation table(id             int,
+                         description_en varchar(15),
+                         description_pt varchar(15));
 
-if not exists(select vc.id
-                from fipe.tb_vehicle_classification vc
-               where vc.id = 2)
-begin
-  insert into fipe.tb_vehicle_classification(id,
-                                             description_en,
-                                             description_pt)
-                                      values(2,
-                                             'Motocycle',
-                                             'Moto');
-end;
-go
+set nocount on;
 
-if not exists(select vc.id
-                from fipe.tb_vehicle_classification vc
-               where vc.id = 3)
-begin
-  insert into fipe.tb_vehicle_classification(id,
-                                             description_en,
-                                             description_pt)
-                                      values(3,
-                                             'Truck',
-                                             'Caminhão');
-end;
-go
+insert into @classification values(1, 'Car', 'Passeio');
+insert into @classification values(2, 'Motocycle', 'Moto');
+insert into @classification values(3, 'Truck', 'Caminhão');
 
-if not exists(select ft.id
-                from fipe.tb_fuel_type ft
-               where ft.id = 1)
-begin
-  insert into fipe.tb_fuel_type(id,
-                                description_en,
-                                description_pt)
-                         values(1,
-                                'Gasoline',
-                                'Gasolina');
-end;
-go
+insert into @fuel_type values(1, 'Gasoline', 'Gasolina');
+insert into @fuel_type values(2, 'Diesel', 'Diesel');
+insert into @fuel_type values(3, 'Alcohol', 'Álcool');
+insert into @fuel_type values(4, 'Unidentified', 'Não Identificado');
+insert into @fuel_type values(5, 'Other', 'Outros');
 
-if not exists(select ft.id
-                from fipe.tb_fuel_type ft
-               where ft.id = 2)
-begin
-  insert into fipe.tb_fuel_type(id,
-                                description_en,
-                                description_pt)
-                         values(2,
-                                'Diesel',
-                                'Diesel');
-end;
-go
+insert into @situation values(1, 'Pending', 'Pendente');
+insert into @situation values(2, 'Incomplete', 'Incompleto');
+insert into @situation values(3, 'Complete', 'Completo');
 
-if not exists(select ft.id
-                from fipe.tb_fuel_type ft
-               where ft.id = 3)
-begin
-  insert into fipe.tb_fuel_type(id,
-                                description_en,
-                                description_pt)
-                         values(3,
-                               'Alcohol',
-                               'Álcool');
-end;
-go
+set nocount off;
 
-if not exists(select ft.id
-                from fipe.tb_fuel_type ft
-               where ft.id = 4)
-begin
-  insert into fipe.tb_fuel_type(id,
-                                description_en,
-                                description_pt)
-                         values(4,
-                                'Unidentified',
-                                'Não Identificado');
-end;
-go
+insert into fipe.tb_vehicle_classification(id,
+                                           description_en,
+                                           description_pt)
+select c.id,
+       c.description_en,
+       c.description_pt
+  from @classification c
+ where not exists(select 1
+                    from fipe.tb_vehicle_classification tvc with(nolock)
+                   where tvc.id = c.id);
 
-if not exists(select tf.id
-                from fipe.tb_fuel_type tf
-               where tf.id = 5)
-begin
-  insert into fipe.tb_fuel_type(id,
-                                description_en,
-                                description_pt)
-                         values(5,
-                                'Other',
-                                'Outros');
-end;
-go
+insert into fipe.tb_fuel_type(id,
+                              description_en,
+                              description_pt)
+select ft.id,
+       ft.description_en,
+       ft.description_pt
+  from @fuel_type ft
+ where not exists(select 1
+                    from fipe.tb_fuel_type tft with(nolock)
+                   where tft.id = ft.id);
 
-if not exists(select rs.id
-                from fipe.tb_reference_situation rs
-               where rs.id = 1)
-begin
-  insert into fipe.tb_reference_situation(id,
-                                          description_en,
-                                          description_pt)
-                                   values(1,
-                                          'Pending',
-                                          'Pendente');
-end;
-go
-
-if not exists(select rs.id
-                from fipe.tb_reference_situation rs
-               where rs.id = 2)
-begin
-  insert into fipe.tb_reference_situation(id,
-                                          description_en,
-                                          description_pt)
-                                   values(2,
-                                          'Incomplete',
-                                          'Incompleto');
-end;
-go
-
-if not exists(select rs.id
-                from fipe.tb_reference_situation rs
-               where rs.id = 3)
-begin
-  insert into fipe.tb_reference_situation(id,
-                                          description_en,
-                                          description_pt)
-                                   values(3,
-                                          'Complete',
-                                          'Completo');
-end;
-go 
+insert into fipe.tb_reference_situation(id,
+                                        description_en,
+                                        description_pt)
+select s.id,
+       s.description_en,
+       s.description_pt
+  from @situation s
+ where not exists(select 1
+                    from fipe.tb_reference_situation trs with(nolock)
+                   where trs.id = s.id);
